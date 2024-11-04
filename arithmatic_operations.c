f2cdf77912de2a1d00b2769c7bb2dae13dea4df5
Vaishnavishiv1 <147625683+Vaishnavishiv1@users.noreply.github.com>
2024-11-04 12:33:54 +0530

    Add files via upload

---
+++ arithmatic_operations.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int a=12, b=4;
+    int sum=a+b;
+    int diff=a-b;
+    int mul=a*b;
+    int div=a/b;
+    int mod=a%b;
+    printf("%d\n",sum);
+    printf("%d\n",diff);
+    printf("%d\n",mul);
+    printf("%d\n",div);
+    printf("%d\n",mod);
+    return 0;
+    }
