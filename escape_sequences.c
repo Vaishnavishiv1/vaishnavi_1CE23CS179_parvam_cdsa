34917c5c6514d5964670c9e2b04d0183aba19fc3
Vaishnavishiv1 <147625683+Vaishnavishiv1@users.noreply.github.com>
2024-11-05 10:03:33 +0530

    Add files via upload

--- escape_sequences.c
+++ escape_sequences.c
@@ -3,6 +3,8 @@ int main()
 {
     printf("demonstrating sequences: \\\\\n");
     //new line
-    printf("the sequence of the line\\\\\\" "the sequence of \\\\\\" "second line" );
+    printf("the sequence of the line\\\\\\" "the sequence of \\\\\\" "second line\n" );
+    printf("helloooooo\n");
+    printf("hiiiiiiiiii");
     return 0;
     }
